d58080339091753a80a47f1ab5d16e5573ac3dd4
pfeyzer <eyad.ebrahem@student.giu-uni.de>
2025-11-13 22:56:15 +0200

    mongodb connection string

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 import { LeavesModule } from './leaves/leaves.module';
 
 @Module({
-  imports: [LeavesModule],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_URI || ''),
+    LeavesModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
